08e6d606936021dc5ea2cba78270c9ea58a6609a
irisu01 <nekomarisakirisame@gmail.com>
2022-11-14 14:54:12 -0500

    add administrator permission

--- src/util/Permissions.ts
+++ src/util/Permissions.ts
@@ -1,25 +1,26 @@
 export enum Permissions {
     // SITE OWNER PERMISSIONS
-    MANAGE_WEBSITE = 1 << 0,
-    DELETE_ACCOUNTS = 1 << 1, // This is mine, and mine alone.
+    ADMINISTRATOR = 1 << 0,
+    MANAGE_WEBSITE = 1 << 1,
+    DELETE_ACCOUNTS = 1 << 2, // This is mine, and mine alone.
 
     // SITE ADMINISTRATOR PERMISSIONS
-    MANAGE_ACCOUNTS = 1 << 2,
+    MANAGE_ACCOUNTS = 1 << 3,
 
     // SITE MODERATOR PERMISSIONS
-    MANAGE_GROUPS = 1 << 3,
-    BAN_ACCOUNTS = 1 << 4,
+    MANAGE_GROUPS = 1 << 4,
+    BAN_ACCOUNTS = 1 << 5,
 
     // LIST OWNER PERMISSIONS
-    DELETE_CHALLENGES = 1 << 5, // Dangerous permissions are reserved for the most trusted people.
+    DELETE_CHALLENGES = 1 << 6, // Dangerous permissions are reserved for the most trusted people.
 
     // LIST MODERATOR PERMISSIONS
-    MANAGE_CHALLENGES = 1 << 6,
-    MANAGE_PLAYERS = 1 << 7,
-    MANAGE_USER_GROUPS = 1 << 8, // Shared with Site Moderator and above, but limited to roles lower in priority.
-    TIMEOUT_ACCOUNTS = 1 << 9, // Shared with Site Moderator
+    MANAGE_CHALLENGES = 1 << 7,
+    MANAGE_PLAYERS = 1 << 8,
+    MANAGE_USER_GROUPS = 1 << 9, // Shared with Site Moderator and above, but limited to roles lower in priority.
+    TIMEOUT_ACCOUNTS = 1 << 10, // Shared with Site Moderator
 
     // LIST HELPER PERMISSIONS
-    MANAGE_RECORDS = 1 << 10,
-    MANAGE_SUBMITTERS = 1 << 11, 
+    MANAGE_RECORDS = 1 << 11,
+    MANAGE_SUBMITTERS = 1 << 12
 }
