1cb990384cb9b4874ebf432098f0448fc3f1ae5f
irisu01 <nekomarisakirisame@gmail.com>
2022-11-14 15:37:51 -0500

    add create group script & script handler

---
+++ scripts/createGroup.js
@@ -0,0 +1,65 @@
+const inquirer = require("inquirer");
+const { PrismaClient } = require("@prisma/client");
+
+let client = new PrismaClient();
+module.exports = {
+    name: "Create group script",
+    execute: () => {
+        // Inquire the user about the group in question.
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the name of the group?"
+            },
+            {
+                type: "input",
+                name: "color",
+                message: "What is the color of the group?",
+                default: "#FFFFFF"
+            },
+            {
+                type: "input",
+                name: "background_color",
+                message: "What is the background color of the group?",
+                default: "#000000"
+            },
+            {
+                type: "input",
+                name: "priority",
+                message: "What is the priority of the group?",
+                default: "0"
+            }
+        ]).then(answers => {
+            // Create the group.
+            client.group.create({
+                data: {
+                    name: answers.name,
+                    color: answers.color,
+                    background_color: answers.background_color,
+                    priority: parseInt(answers.priority)
+                }
+            }).then(group => {
+                // Empty line.
+                console.log("");
+
+                // Log the group.
+                console.log("Group created:");
+                console.log(group);
+
+                // Exit the script.
+                process.exit();
+            }).catch(error => {
+                // Empty line.
+                console.log("");
+
+                // Log the error.
+                console.log("Error creating group:");
+                console.log(error);
+
+                // Exit the script.
+                process.exit();
+            });
+        });
+    }
+}
---
+++ scripts/scriptHandler.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// Import all the scripts.
+const scripts = []
+
+for (let file of fs.readdirSync(__dirname)) {
+    if (file.endsWith(".js")) {
+        if (file === "scriptHandler.js") {
+            // Don't import this file.
+            continue;
+        }
+
+        scripts.push(require(__dirname + "/" + file));
+    }
+}
+
+// Ask the user which script they want to run.
+inquirer.prompt([
+    {
+        type: "list",
+        name: "script",
+        message: "Which script do you want to run?",
+        choices: scripts.map(script => script.name)
+    }
+]).then(answers => {
+    // Find the script object.
+    const script = scripts.find(script => script.name === answers.script);
+
+    // Empty line.
+    console.log("");
+
+    // Run the script.
+    script.execute();
+});
